fc509d35cf6808b83da397845c2426b1e25d20e3
Haseeb Ahmed <haseeb.centrox@gmail.com>
2022-07-11 02:06:50 +0500

    added more packages

--- App.tsx
+++ App.tsx
@@ -4,7 +4,6 @@ import * as React from 'react';
 import {View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 function HomeScreen() {
   return (
